f6bb6127b47035062e42bbffe95b3581077b2060
Daniel Kim <danielkim802@yahoo.com>
2016-11-27 19:01:09 -0500

    finished bullet.ml, but might have to change Bot.t later

--- bullet.ml
+++ bullet.ml
@@ -1,39 +1,6 @@
-module type Bullet = sig
-  (* The type of a bullet that has a position, velocity, and owner *)
-  type t
-
-  (* The type of the owner of the bullet, i.e. the bot that shot it *)
-  type bot
-
-  (* Gets the Position of the bullet 
-   * returns A 2D Tuple of the position in x,y *)
-  val getPosition : t -> float * float
-
-  (* Gets the Velocity of the bullet
-   * returns A 2D Tuple of the velocity in x,y *)
-  val getVelocity : t -> float * float
-
-  (* Gets the Speed of the bullet *)
-  val getSpeed : t -> float
-
-  (* Gets the bot that shot the bullet *)
-  val getOwner : t -> bot
-
-  (* Makes a new bullet with a given position, velocity, and owner
-   * [make (xPos,yPos) (xVel,yVel) owner] *)
-  val make : (float * float) -> (float * float) -> bot -> t
-
-  (* Updates the bullet for a single logic tick *)
-  val step : t -> t
+module Bot = struct 
+  type t = int      (* can change this later *)
 end
-
-module type Bot = sig
-  (* The type of Bot *)
-  type t
-end
-
-module type MakeBullet =
-  functor (B : Bot) -> Bullet with type bot = B.t
 
 module MakeBullet (B : Bot) = struct
   type bot = B.t
@@ -48,21 +15,33 @@ module MakeBullet (B : Bot) = struct
   }
 
   let getPosition t =
-    failwith "Unimplemented"
+    (t.xPos, t.yPos)
 
   let getVelocity t =
-    failwith "Unimplemented"
+    (t.xVel, t.yVel)
 
   let getSpeed t =
-    failwith "Unimplemented"
+    t.speed
 
   let getOwner t =
-    failwith "Unimplemented"
+    t.owner
 
-  let make (xPos,yPos) (xVel,yVel) bot =
-    failwith "Unimplemented"
-
-  let step t =
-    failwith "Unimplemented"
+  let make (xPos,yPos) (xVel,yVel) bot = {
+    xPos = xPos;
+    yPos = yPos;
+    xVel = xVel;
+    yVel = yVel;
+    speed = 0;
+    owner = bot;
+  }
+    
+  let step t = {
+    xPos = t.xPos+t.xVel;
+    yPos = t.yPos+t.yVel;
+    xVel = t.xVel;
+    yVel = t.yVel;
+    speed = t.speed;
+    owner = t.owner;    
+  }
 
 end
