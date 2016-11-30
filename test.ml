7fc06f9bf5fddb68f60f8c02426e1e8da02fe235
Jack Solon <jps386@cornell.edu>
2016-11-29 19:04:04 -0500

    fixed syntax errors in test.ml. Tests currently fail

--- test.ml
+++ test.ml
@@ -1,42 +1,43 @@
 open OUnit2
 open Bot
 open Api
+open Game
 
-let h1 = Bot.make (10.,10.) (1.,0.) 10. 10. (fun _ -> Api.turnLeft 90.)
+let b1 = Bot.make (10.,10.) (1.,0.) 10. 10. (fun _ -> Api.turnLeft 90.)
 
-let h2 = Bot.make (10.,10.) (1.,0.) 10. 10. (fun _ -> Api.turnLeft 90.)
+let b2 = Bot.make (10.,10.) (1.,0.) 10. 10. (fun _ -> Api.turnLeft 90.)
 
-let h3 = Bot.make (10.,10.) (1.,0.) 10. 10. (fun _ -> Api.turnLeft 90.)
+let b3 = Bot.make (10.,10.) (1.,0.) 10. 10. (fun _ -> Api.turnLeft 90.)
 
 let tests = "test suite" >::: [
   "Forward1" >::
     (fun _ -> assert_equal
       ((20.,10.))
-      (let _ = Bot.execute (h1) (makeCommand "Forward" 1.) in getPosition h1));
+      (let _ = Game.execute (b1) (Api.forward 1.) in getPosition b1));
   "Step1" >::
     (fun _ -> assert_equal
-      ((6.12323399573676604e-17, 1.))
-      (step (); getDirection h1));
+      ((1.,0.))(* (6.12323399573676604e-17, 1.)) *)
+      (step (); getDirection b1));
   "Step2" >::
     (fun _ -> assert_equal
       ((-1.83697019872102969e-16, -1.))
-      (step (); step (); step (); getDirection h1));
+      (step (); step (); step (); getDirection b1));
   "LT1" >::
     (fun _ -> assert_equal
       ((-1., 1.22464679914735321e-16))
-      (let _ = Bot.execute (h2) (makeCommand "LT" 180.) in getDirection h2));
+      (let _ = Game.execute (b2) (Api.turnLeft 180.) in getDirection b2));
   "RT1" >::
     (fun _ -> assert_equal
       ((-1., 1.22464679914735321e-16))
-      (let _ = Bot.execute (h3) (makeCommand "RT" 180.) in getDirection h3));
+      (let _ = Game.execute (b3) (Api.turnRight 180.) in getDirection b3));
   "Shoot" >::
     (fun _ -> assert_equal
       (1)
-      (let _ = Bot.execute (h3) (makeCommand "Shoot" 0.) in num_bullet ()));
+      (let _ = Game.execute (b3) (Api.shoot()) in List.length (getBullets())));
   "NoCmd" >::
     (fun _ -> assert_equal
-      (())
-      (Bot.execute (h3) (makeCommand "foobar" 0.)));
+      (b3)
+      (Game.execute (b3) (Api.wait())));
 ]
 
 let _ = run_test_tt_main tests
