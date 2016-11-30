7486a2ab5e8a25fcb7a98f2b8248a79a8d210f3e
Jack Solon <jps386@cornell.edu>
2016-11-29 20:53:55 -0500

    fixed tests in Test.ml. All tests now pass

--- test.ml
+++ test.ml
@@ -4,40 +4,76 @@ open Api
 open Game
 
 let b1 = Bot.make (10.,10.) (1.,0.) 10. 10. (fun _ -> Api.turnLeft 90.)
+let _ = Game.init [(fun _ -> Api.turnLeft 90.)] 1 1.
+let lb = getBots()
+let direc = List.hd lb |> getDirection
+let get_x direc = let (x,y) = direc in x
+let get_y direc = let (x,y) = direc in y
 
-let b2 = Bot.make (10.,10.) (1.,0.) 10. 10. (fun _ -> Api.turnLeft 90.)
+(* Used as function to ~printer to pring out a pair of floats*)
+let string_of_tuple tup = 
+  let (x,y) = tup in
+  "("^(string_of_float x)^","^(string_of_float y)^")"
 
-let b3 = Bot.make (10.,10.) (1.,0.) 10. 10. (fun _ -> Api.turnLeft 90.)
+(* Helper functions to test step *)
+let pi = 3.14159265358979324
+
+let toRad (deg : float) = 
+  ((mod_float deg 360.)/.360.) *. 2. *. pi
+
+let turn_left deg (x1,y1) = 
+    let theta' = mod_float ((atan2 y1 x1) +. toRad deg) (2. *. pi) in
+    (cos theta', sin theta')
 
 let tests = "test suite" >::: [
   "Forward1" >::
     (fun _ -> assert_equal
-      ((20.,10.))
-      (let _ = Game.execute (b1) (Api.forward 1.) in getPosition b1));
-  "Step1" >::
+      ~printer: string_of_float 20.
+      (let temp = Game.execute (b1) (Api.forward 1.) in get_x (getPosition temp)));
+  "Step1a" >::
     (fun _ -> assert_equal
-      ((1.,0.))(* (6.12323399573676604e-17, 1.)) *)
-      (step (); getDirection b1));
-  "Step2" >::
+      true
+      (cmp_float
+      ~epsilon: 0.000001
+      (get_x (turn_left 90. direc))
+      (step (); get_x (getDirection (List.hd (getBots()))))));
+  "Step1b" >::
     (fun _ -> assert_equal
-      ((-1.83697019872102969e-16, -1.))
-      (step (); step (); step (); getDirection b1));
+      true
+      (cmp_float
+      ~epsilon: 0.000001
+      (get_y (turn_left 90. direc))
+      (step (); get_y (getDirection (List.hd (getBots()))))));
+  "Step2a" >::
+    (fun _ -> assert_equal
+      true
+      (cmp_float
+      ~epsilon: 0.000001
+      (get_x (turn_left 360. direc))
+      (step (); step (); step (); get_x (getDirection (List.hd (getBots()))))));
+  "Step2b" >::
+    (fun _ -> assert_equal
+      true
+      (cmp_float
+      ~epsilon: 0.000001
+      (get_y (turn_left 360. direc))
+      (step (); step (); step (); get_y (getDirection (List.hd (getBots()))))));
   "LT1" >::
     (fun _ -> assert_equal
-      ((-1., 1.22464679914735321e-16))
-      (let _ = Game.execute (b2) (Api.turnLeft 180.) in getDirection b2));
+      ~printer: string_of_float (-1.)
+      (let temp = Game.execute (b1) (Api.turnLeft 180.) in let (x,y) = getDirection temp in x));
   "RT1" >::
     (fun _ -> assert_equal
-      ((-1., 1.22464679914735321e-16))
-      (let _ = Game.execute (b3) (Api.turnRight 180.) in getDirection b3));
+      ~printer: string_of_float (-1.)
+      (let temp = Game.execute (b1) (Api.turnRight 180.) in let (x,y) = getDirection temp in x));
   "Shoot" >::
     (fun _ -> assert_equal
       (1)
-      (let _ = Game.execute (b3) (Api.shoot()) in List.length (getBullets())));
+      (let _ = Game.execute (b1) (Api.shoot()) in List.length (getBullets())));
   "NoCmd" >::
     (fun _ -> assert_equal
-      (b3)
-      (Game.execute (b3) (Api.wait())));
+      (getID b1)
+      (Game.execute (b1) (Api.wait()) |> getID));
 ]
 
 let _ = run_test_tt_main tests
