8cee8d4d9d0621b63993385ead3a2308699d5e37
Daniel Kim <danielkim802@yahoo.com>
2016-12-04 07:25:21 -0500

    changed file structure

--- ai.mli
+++
@@ -1,7 +0,0 @@
-open Api
-
-(* A single AI tick. This will be implemented by the player, and will use
- * functions from api.mli *)
-val step : Bot.t -> Bot.command
-
-val step2 : Bot.t -> Bot.command
