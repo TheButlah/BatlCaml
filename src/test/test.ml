6644744f7d6511e06b467bf752cf8399788903d8
Ryan Butler <thebutlah@gmail.com>
2016-12-04 22:49:24 -0500

    Fixed order of fuction argument application in unit tests.

--- src/test/test.ml
+++ src/test/test.ml
@@ -1,15 +1,10 @@
 open OUnit2
-open Bot
 open Api
-open Game
 
 let setupGame () =
   Game.init [(fun _ -> Api.turnLeft 90.)] 1 1. 1. 1. 1. 1. 1. 1. 1.;
   Bot.make (10.,10.) (1.,0.) 10. 10. 1. 1. (fun _ -> Api.turnLeft 90.)
 
-let direc () = 
-  let lb = getBots() in
-  List.hd lb |> getDirection
 
 let get_x = fst
 let get_y = snd
@@ -19,21 +14,20 @@ let string_of_tuple tup =
   let (x,y) = tup in
   "("^(string_of_float x)^","^(string_of_float y)^")"
 
+let direc () = 
+  let lb = Game.getBots() in
+  List.hd lb |> getDirection
+
 (* Helper functions to test step *)
 let pi = 4. *. atan 1.
 
 (* Returns -2pi to 2pi exclusive*)
 let toRad (deg : float) = 
-  let result = ((mod_float deg 360.)/.360.) *. 2. *. pi in
-  print_endline ("toRad: " ^ string_of_float deg ^ " -> " ^ string_of_float result);
-  result
+  ((mod_float deg 360.)/.360.) *. 2. *. pi
 
 let turn_left deg (x1,y1) =
-
   let theta' = mod_float ((atan2 y1 x1) +. toRad deg) (2. *. pi) in
-  let (x,y) = (cos theta', sin theta') in
-  print_endline ("turn_left " ^ string_of_float deg^" "^string_of_tuple (x1,y1)^" -> "^string_of_tuple (x,y));
-  (x,y)
+  (cos theta', sin theta')
 
 let tests = "test suite" >::: [
   "Forward1x" >::
@@ -41,46 +35,45 @@ let tests = "test suite" >::: [
       let b1 = setupGame () in
       assert_equal ~printer: string_of_float 
       20.
-      (Game.execute (b1) (Api.forward 1.) |> getPosition |> get_x));
+      (Game.execute (b1) (Api.forward 1.) |> Bot.getPosition |> get_x));
   "Forward1y" >::
-    (fun _ -> 
+    (fun _ ->
       let b1 = setupGame () in
       assert_equal ~printer: string_of_float 
       10.
-      (Game.execute (b1) (Api.forward 1.) |> getPosition |> get_y));
+      (Game.execute (b1) (Api.forward 1.) |> Bot.getPosition |> get_y));
   "Step1x" >::
     (fun _ -> 
       let _ = setupGame () in
       direc () |> string_of_tuple |> print_endline;
-      assert_equal ~printer: string_of_float
+      let e1 = (direc () |> turn_left 90. |> get_x ) in
+      let e2 = (Game.step (); direc () |> get_x) in
+      assert_equal ~printer:string_of_float e1 e2);
   (*    true
       (cmp_float ~epsilon: 0.0001*)
-      (direc () |> turn_left 90. |> get_x )
-      (step (); direc () |> get_x));
   "Step1y" >::
-    (fun _ -> 
+    (fun _ ->
       let _ = setupGame() in
-      assert_equal ~printer: string_of_float
+      let e1 = (direc () |> turn_left 90. |> get_y ) in
+      let e2 = (Game.step (); direc () |> get_y) in
+      assert_equal ~printer: string_of_bool 
+      true (cmp_float ~epsilon: 0.000001 e1 e2));
    (*   true
       (cmp_float ~epsilon: 0.01 *)
-      (direc () |> turn_left 90. |> get_y )
-      (step (); direc () |> get_y));
   "Step2x" >::
     (fun _ -> 
       let _ = setupGame () in
+      let e1 = (direc () |> turn_left 360. |> get_x) in
+      let e2 = Game.(step (); step (); step (); step (); direc () |> get_x) in
       assert_equal ~printer: string_of_bool
-      true
-      (cmp_float ~epsilon: 0.01
-      (direc () |> turn_left 360. |> get_x)
-      (step (); step (); step (); step (); direc () |> get_x)));
+      true (cmp_float ~epsilon: 0.000001 e1 e2));
   "Step2y" >::
     (fun _ -> 
       let _ = setupGame () in
+      let e1 = (direc () |> turn_left 360. |> get_y) in
+      let e2 = Game.(step (); step (); step (); step (); direc () |> get_y) in
       assert_equal ~printer: string_of_bool
-      true
-      (cmp_float ~epsilon: 0.000001
-      (direc () |> turn_left 360. |> get_y)
-      (step (); step (); step (); step (); direc () |> get_y)));
+      true (cmp_float ~epsilon: 0.000001 e1 e2));
   "LT1" >::
     (fun _ -> 
       let b1 = setupGame() in
@@ -98,13 +91,13 @@ let tests = "test suite" >::: [
       let b1 = setupGame () in
       assert_equal ~printer: string_of_int
       1
-      (Game.execute (b1) (Api.shoot()) |> ignore ; getBullets () |> List.length));
+      (Game.execute (b1) (Api.shoot()) |> ignore ; Game.getBullets () |> List.length));
   "Wait" >::
     (fun _ -> 
       let b1 = setupGame () in
-      assert_equal ~printer: string_of_int
-      (getID b1)
-      (Game.execute (b1) (Api.wait()) |> getID));
+      let e1 = (getID b1) in
+      let e2 = (Game.execute (b1) (Api.wait()) |> getID) in
+      assert_equal ~printer: string_of_int e1 e2);
 ]
 
 let _ = run_test_tt_main tests
